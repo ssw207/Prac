0584e9f9c462cb3973715f841c19a15d35a26729
swsong <swsong@DESKTOP-DPCBADD>
2018-09-12 07:27:52 +0900

    Service로 비즈니스 로직 분리

---
+++ Prac1/src/main/java/com/song/woo/ctrl/HomeController.java
@@ -0,0 +1,51 @@
+package com.song.woo.ctrl;
+
+import java.io.UnsupportedEncodingException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.xml.ws.Response;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.song.woo.model.IService;
+
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	@Autowired
+	private IService service;
+	
+//	시작페이지로 이동
+	@RequestMapping(value = "/home.do", method = RequestMethod.GET)
+	public String move(Locale locale, Model model ) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		String a="아이고";
+		model.addAttribute("a",a);
+		return "home";
+	}
+	
+	
+	@RequestMapping(value = "/watch.do",produces = "application/text; charset=utf8", method = RequestMethod.GET)//주소만 보내므로 get방식으로 통신,produeces사용하여 Http헤더의 컨텐트타입을 utf-8로 변경
+	@ResponseBody//return값을 ajax로 처리하기위해 선언 
+	public String watch(){//입력값이 없으므로 파라미터 선언하지 않음
+		String result=service.watch();
+		return result;//결과값 반환   
+	}
+	
+}
--- Prac1/src/main/webapp/WEB-INF/views/home.jsp
+++ Prac1/src/main/webapp/WEB-INF/views/home.jsp
@@ -1,32 +1,33 @@
-<%@ page pageEncoding="UTF-8" contentType="text/html; charset=UTF-8"%> 
-
-
+<%@ page pageEncoding="UTF-8" contentType="text/html; charset=UTF-8"%>
+<%-- <% request.setCharacterEncoding("utf-8"); %> --%>
+<%-- <% response.setContentType("text/html; charset=utf-8"); %> --%>
+<!DOCTYPE html>
 <html>
 <head>
 <title>1일차 과제</title>
+<meta charset="UTF-8" />
 </head>
-<%
-    request.setCharacterEncoding("UTF-8");
-%>
-
+	
 <script type="text/javascript" src="./js/jquery-3.3.1.js"></script>
 <script type="text/javascript" >
-function test(){//
+function test(){
 	setInterval(function() {//시작버튼 클릭시 작동
 		$.ajax({//아작스통신 시작
 			type : "GET",//주소만 보내므로 get통신
  			//data: "",//입력값없음로 사용하지 않음
 			url : "./watch.do",//컨트롤러의 리퀘스트맵핑과 일치하는 명령어
 			success : function(result) {//아작스통신으로 컨트롤러에서 결과값을 result로 받아옴
-				$('#time').html(result.result);//돔탐색을하여 id가 time인 위치를 찾고 바닥에 아작스통신으로 받아온 결과값을찍음
+				$('#time').html(result);//돔탐색을하여 id가 time인 위치를 찾고 바닥에 아작스통신으로 받아온 결과값을찍음
 			}
 		})
 	}, 1000);//1초간격으로 반복
 
 }
+
+
 </script>
 <body>
-	<h1>1일차 과제</h1>
+	<h1>1차 과제 현재시간 ajax로 출력하기</h1>
 	<div id="time"></div>
 	<input type="button" value="시작" onclick="test()">
 </body>
